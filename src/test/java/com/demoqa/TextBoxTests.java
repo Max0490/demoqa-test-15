aa2791ec30416e8bc8b031bec78acc9ffaf82a30
Max0490 <seagate90@mail.ru>
2022-09-15 22:35:00 +0300

    add Selenide

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,23 @@
+package com.demoqa;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.lang.module.Configuration;
+
+public class TextBoxTests {
+
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl =
+        Configuration.browserSize
+
+    }
+
+    @Test
+    void fillFormTest() {
+
+    }
+}
