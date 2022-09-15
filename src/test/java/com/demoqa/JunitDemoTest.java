6ced52274460c6007372a26823cf38c88a4ec04b
Max0490 <seagate90@mail.ru>
2022-09-15 21:50:28 +0300

    add tests and conditions

--- src/test/java/com/demoqa/JunitDemoTest.java
+++ src/test/java/com/demoqa/JunitDemoTest.java
@@ -1,16 +1,38 @@
 package com.demoqa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JunitDemoTest {
 
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll !");
+    }
+
+    @AfterAll
+    static void  tearDown() {
+        System.out.println("### @AfterAll !");
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        // open("ya.ru")
+        System.out.println("### @BeforeEach !");
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        System.out.println("### @AfterEach !");
+    }
+
     @Test
     void simpleTestFirst() {
+        System.out.println("### @Test simpleTestFirst() !");
         Assertions.assertTrue(3>2);
     }
     @Test
     void simpleTestSecond() {
+        System.out.println("### @Test simpleTestSecond() !");
         Assertions.assertTrue(3>2);
     }
 }
