d7576d4b9e25447a8a81c12717631d71d45708a8
Max0490 <seagate90@mail.ru>
2022-09-16 19:46:01 +0300

    Css

--- src/test/java/com/demoqa/CssXpathExample.java
+++ src/test/java/com/demoqa/CssXpathExample.java
@@ -1,6 +1,8 @@
 package com.demoqa;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CssXpathExample {
     $("[data-testid=email]").setValue("a@a.com")
